a5993629b0fa8fe6a8c959f553a57a2b065718f2
Mohammad Shafiee <51268970+wikm360@users.noreply.github.com>
2024-02-18 20:59:01 +0330

    Update fragmentbot.py
    change method
    change site
    add protocol , uuid

--- py/fragmentbot.py
+++ py/fragmentbot.py
@@ -1,21 +1,44 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import pyperclip
 
+#Menu
+vless = "/html/body/div[2]/div/div[6]/select[1]/option[1]"
+vmess = "/html/body/div[2]/div/div[6]/select[1]/option[2]"
+trojan = "/html/body/div[2]/div/div[6]/select[1]/option[3]"
+option = input("Enter your Protocol : ")
+if option == "1" :
+    option2 = vless
+if option == "2" :
+    option2 = vmess
+if option == "3" :
+    option2 = trojan
+uuid = input("Enter ypur UUID : ")
 
 # Start a new instance of Chrome web browser
 driver = webdriver.Chrome()
-Text = "vless://890ce5ff-a200-45d4-bbed-7de6c48e8920@wikm1.ddnsking.com:2083?security=tls&type=ws&headerType=&path=wikm2083&host=media_gateway-control-protocol.mabasite.ir&sni=hyper-text-transfer-protocol.mabasite.ir&fp=chrome&alpn=#CDN-MCI"
+
 # Open the website
-driver.get("https://ircfspace.github.io/fragment")
+driver.get("https://wikm.ir/frag/")
+
+# Find and click the option that reveals additional options
+protocol = driver.find_element(By.ID,"protocol")
+protocol.click()
+
+# Wait , find and click on option
+wait = WebDriverWait(driver, 10)
+specific_option = driver.find_element(By.XPATH,option2)
+specific_option.click()
 
 # Find the input field using its ID 
-input_field = driver.find_element(By.ID,"defConfig")
+input_field = driver.find_element(By.ID,"uuid")
 
 # Type text into the input field
 input_field.clear()
-input_field.send_keys(Text)
+input_field.send_keys(uuid)
 
 # Find the button element by its ID 
 check = driver.find_element(By.ID,"checkConf")
