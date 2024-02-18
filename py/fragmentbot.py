4d90faf5a4ec26237fbe4bd6280bf04a3961c293
Mohammad Shafiee <51268970+wikm360@users.noreply.github.com>
2024-02-18 21:18:26 +0330

    Update fragmentbot.py
    optimize and add stream

--- py/fragmentbot.py
+++ py/fragmentbot.py
@@ -9,13 +9,21 @@ import pyperclip
 vless = "/html/body/div[2]/div/div[6]/select[1]/option[1]"
 vmess = "/html/body/div[2]/div/div[6]/select[1]/option[2]"
 trojan = "/html/body/div[2]/div/div[6]/select[1]/option[3]"
-option = input("Enter your Protocol : ")
-if option == "1" :
-    option2 = vless
-if option == "2" :
-    option2 = vmess
-if option == "3" :
-    option2 = trojan
+ws = "/html/body/div[2]/div/div[6]/select[2]/option[1]"
+grpc = "/html/body/div[2]/div/div[6]/select[2]/option[2]"
+
+option = input("Enter your Protocol (vless=l , vmess=m , trojan=t) : ")
+if option == "l" :
+    option = vless
+if option == "m" :
+    option = vmess
+if option == "t" :
+    option = trojan
+transmit = input("Enter ws or grpc : ")
+if transmit == ws : 
+    transmit = ws
+else : 
+    transmit = grpc
 uuid = input("Enter ypur UUID : ")
 
 # Start a new instance of Chrome web browser
@@ -30,13 +38,18 @@ protocol.click()
 
 # Wait , find and click on option
 wait = WebDriverWait(driver, 10)
-specific_option = driver.find_element(By.XPATH,option2)
+specific_option = driver.find_element(By.XPATH,option)
 specific_option.click()
 
-# Find the input field using its ID 
-input_field = driver.find_element(By.ID,"uuid")
+# find , wait, click transmit
+stream = driver.find_element(By.ID,"stream")
+stream.click()
+wait = WebDriverWait(driver,10)
+stream = driver.find_element(By.XPATH,transmit)
+stream.click()
 
-# Type text into the input field
+# Find , type uuid
+input_field = driver.find_element(By.ID,"uuid")
 input_field.clear()
 input_field.send_keys(uuid)
 
